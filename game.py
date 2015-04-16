1e4884f112665d7dd29d801293b7c3f6642b8759
Ricardo Paiva <ricardo.paiva@corp.globo.com>
2015-04-16 15:47:03 -0300

    Primeiros testes de interface

--- game.py
+++ game.py
@@ -1,35 +1,44 @@
+from __future__ import print_function
 from blackjack.blackjack_game import BlackjackGame, GameBustedException
-import sys
 
-def main():
-    game = BlackjackGame(shuffle=True)
-    while True:
-        sys.stdout.write("My cards: " + str(game.my_cards)+"\n")
-        print "Dealer cards: ", game.dealer_cards[0]
-        option = raw_input('Hit(h) or Stand(s) or Quit(q): ')
-        if option == "s":
-            game.stand()
-            print "Dealer cards: ", game.dealer_cards
-            print "My cards: ", game.my_cards
-            result = game.win()
-            if result == BlackjackGame.WIN:
-                print "You win!" 
-            elif result == BlackjackGame.LOSE:
-                print "You lose!"
-            else:
-                print "Draw match!"
-            break
-        elif option == "h":
-            try:
-                print "My cards: ", game.my_cards
-                game.hit()
-            except GameBustedException:
-                print "You lose!!"
+def _output(*args):
+    print(*args)
+
+def _input(msg):
+    return raw_input(msg)
+
+class CommandLineGame():
+    
+    @classmethod
+    def main(cls):
+        cls.game = BlackjackGame(shuffle=True)
+        while True:
+            _output("My cards: ", cls.game.my_cards)
+            _output("Dealer cards: ", cls.game.dealer_cards[0])
+            option = _input('Hit(h) or Stand(s) or Quit(q): ').lower()
+            if option == "s":
+                cls.game.stand()
+                _output("Dealer cards: ", cls.game.dealer_cards)
+                _output("My cards: ", cls.game.my_cards)
+                result = cls.game.win()
+                if result == BlackjackGame.WIN:
+                    _output("You win!")
+                elif result == BlackjackGame.LOSE:
+                    _output("You lose!")
+                else:
+                    _output("Draw match!")
                 break
-        elif option == "q":
-            break
-        else:
-            print "Wrong option (s or h)"
+            elif option == "h":
+                try:
+                    _output("My cards: ", cls.game.my_cards)
+                    cls.game.hit()
+                except GameBustedException:
+                    _output("You lose!!")
+                    break
+            elif option == "q":
+                break
+            else:
+                _output("Wrong option (s or h)")
 
 if __name__ == '__main__':
-    main()
+    CommandLineGame.main()
--- test/test_game_interface.py
+++ test/test_game_interface.py
@@ -1,9 +1,30 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+from mock import Mock, call
+import game
+from game import CommandLineGame
 
 
 class TestGameInterface(unittest.TestCase):
 
+    old_output = None
+    old_input = None
+
+    def setUp(self):
+        self.old_output = game._output
+        self.old_input = game._input
+        game._output = Mock()
+        game._input = Mock()
+
+    def tearDown(self):
+        game._output = self.old_output
+        game._input = self.old_input
+
     def test_start_game(self):
-        pass
+        game._input = lambda x: "q"
+        CommandLineGame.main()
+        game._output.assert_has_calls([
+            call("My cards: ", CommandLineGame.game.my_cards),
+            call("Dealer cards: ", CommandLineGame.game.dealer_cards[0])
+        ]) # ("My cards: ")
